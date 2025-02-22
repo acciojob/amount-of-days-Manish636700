a2a0ce7bc2bc70cb374fcaa1d693244f23f70246
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-22 21:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(input){
+	if(input%400===0)
+		return true;
+	else if(input%4===0 && input%100===0)
+		return true;
+	else 
+		return false;
+		
+}
